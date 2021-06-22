223e788cd3701c68596f521de5616ed9e74a8f06
Kyle Ruban <kmruban@coastal.edu>
2021-06-22 02:11:18 -0400

    load products from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -49,7 +49,7 @@ import React from 'react';
                      {product.countInStock > 0 ? (
                        <span className="success">In Stock</span>
                      ) : (
-                       <span className="error">Unavailable</span>
+                      <span className="danger">Unavailable</span>
                      )}
                    </div>
                  </div>
