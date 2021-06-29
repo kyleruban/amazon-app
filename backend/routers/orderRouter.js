68769ab4f7bd71ad0463242f8f86d4776a44264e
Kyle Ruban <kmruban@coastal.edu>
2021-06-29 16:27:53 -0400

    display orders history

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -5,6 +5,16 @@ import express from 'express';
 
  const orderRouter = express.Router();
 
+ orderRouter.get(
+  '/mine',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    //only return orders of the same id
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+
  orderRouter.post(
    '/',
    isAuth,
